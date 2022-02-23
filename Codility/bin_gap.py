594bf7527b6f6d1c0dc4fbfe12f16ff5c33224ef
Kit Bolourchi <kitbolourchi@Kit-Bolourchis-MacBook-Air.local>
2022-02-23 20:33:11 +0000

    Update answer

--- Codility/bin_gap.py
+++ Codility/bin_gap.py
@@ -1,16 +1,24 @@
-num = '10001001001011100100'
+# # Write a function:
+
+# class Solution { public int solution(int N); }
+# that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+
+# For example, given N = 1041 the function should return 5, because N has binary representation 10000010001
+
+n = 1041
 
 def solution(N):
+    bina = bin(N)
     current = 0
     longest = 0
-    for n in N:
-        if int(n) == 0:
+    for b in bina[2:]:
+        if int(b) == 0:
             current += 1
-        elif current >= longest:
-            longest = current
+        elif int(b) == 1:
+            longest = max(current, longest)
             current = 0
-        else:
-            current = 0
+
     return longest
 
-print(solution(num))
+
+print(solution(n))
